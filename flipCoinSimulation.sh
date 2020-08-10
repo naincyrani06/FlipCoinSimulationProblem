7f3d3f14fe70a39240f1833083a0e3927758aece
naincyrani06 <naincyrani06it@gmail.com>
2020-08-11 00:35:49 +0530

    Displaying flipping Coin Of Head and Tail Till Either Of Them Win 21 Times

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -2,19 +2,32 @@
 
 isHeads=1
 isTails=0
-countHeads=0
-countTails=0
-for(( i=1;i<=10;i++ ))
+countHead=0
+countTail=0
+winning=0
+for((i=1;i<=70;i++))
 do
-	flipCheck=$((RANDOM%2))
-	if [ $flipCheck -eq $isHeads ]
+        flipCheck=$((RANDOM%2))
+        if [ $flipCheck -eq $isHead ]
+        then
+                countHead=$((countHead + 1))
+        elif [ $flipCheck -eq $isTail ]
+        then
+                countTail=$((countTail + 1))
+        fi
+
+	if [ $countHead -eq 21 ]
 	then
-		countHeads=$((countHeads + 1))
-	elif [ $flipCheck -eq $isTails ]
-	then
-                countTails=$((countTails + 1))
+		echo "Head count is:$countHead"
+		echo "Tail count is:$countTail"
+		break
+	elif [ $countTail -eq 21 ]
+        then
+		echo "Head count is:$countHead"
+                echo "Tail count is:$countTail"
+		break
 	fi
 done
 
-echo "Total Heads count is: $countHeads"
-echo "Total Tails count is: $countTails"
+echo "Total Number of head count is:$countHead"
+echo "Total Number of tail count is:$countTail"
