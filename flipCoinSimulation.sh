974243db991c1c5a10cd60544e5e7fa6475710f9
naincyrani06 <naincyrani06it@gmail.com>
2020-08-11 00:21:06 +0530

    Looping through Multiple Times Of Flipping To Show Winner

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -x
 
-flipCheck=$((RANDOM%2))
-
 isHeads=1
+isTails=0
+countHeads=0
+countTails=0
+for(( i=1;i<=10;i++ ))
+do
+	flipCheck=$((RANDOM%2))
+	if [ $flipCheck -eq $isHeads ]
+	then
+		countHeads=$((countHeads + 1))
+	elif [ $flipCheck -eq $isTails ]
+	then
+                countTails=$((countTails + 1))
+	fi
+done
 
-echo "Flip Value: "$flipCheck
-
-if [ $flipCheck == $isHeads ]
-then
-	echo "Head is Winner"
-else
-	echo "Tail is Winner"
-fi
+echo "Total Heads count is: $countHeads"
+echo "Total Tails count is: $countTails"
